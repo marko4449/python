d37f45dd357ec05070a2d4e22f1a1c5343f85e5e
Marko Kaja <marko4449@yahoo.com>
2017-09-25 22:43:30 +0300

    py

--- for.py
+++ for.py
@@ -13,12 +13,9 @@ else:
 # list that combines multiple combinations of surnames
 studentF = ['mike', 'dan', 'lucy', 'carmen']
 studentL = ['storm', 'apple', 'green', 'lan']
-
-for f in studentF:
-    for l in studentL:
-        f + l
-        print(f + l)
-
+names = zip(studentF, studentL)
+for a, b in names:
+    print(a, b)
 
 # defined incrementation for loop
 for x in range(5, 10, 4):
@@ -168,7 +165,7 @@ def download(csv_url):
 
 
 download(url)
-'''
+
 # web crawler
 import requests
 from bs4 import BeautifulSoup
@@ -190,3 +187,189 @@ def trade(maxpages):
 
 
 trade()
+
+#try/except/finally
+while True:
+    try:
+        user = int(input("insert number \n"))
+        print(user)
+        break
+    except ValueError:
+        print("yo fool enter a number")
+    except: #not a good idea cuz you wont know what problems would occur
+    	break
+    finally:
+    	print("complete")
+
+
+class Test:
+    book = 3
+
+    def loan(self):
+        print(' you loaned a book')
+        self.book -= 1
+
+    def checkbook(self):
+        if self.book <= 0:
+            print("no more books")
+        else:
+            print(str(self.book))
+
+
+test1 = Test()
+test1.loan()
+test1.checkbook()
+
+
+
+class Test:
+
+    def __init__(self):
+        print('testing')
+
+    def test2(self):
+        print('testing2')
+
+
+tester = Test()
+tester.test2()
+
+
+
+class Bookcase():
+    def __init__(self, x):
+        self.amount = x
+
+    def books(self):
+        print(self.amount)
+
+
+raamat = Bookcase(5)
+raamat2 = Bookcase(20)
+
+raamat.books()
+raamat2.books()
+
+
+class Girl:
+
+    gender = 'female'
+
+    def __init__(self, name):
+        self.name = name
+
+
+r = Girl('Rachel')
+s = Girl('Stacey')
+print(r.gender)
+print(s.gender)
+print(r.name)
+print(s.name)
+
+
+# inheritance
+class Parent():
+
+    def lastname(self):
+        print('ln')
+
+
+class Child(Parent):
+
+    def firstn(self):
+        print('fn')
+
+    def lastname(self):
+        print('ffn')
+
+
+name = Child()
+name.firstn()
+name.lastname()
+
+# multiple inheritance
+
+
+class Figure():
+    def move(self):
+        print('the figure is moving')
+
+
+class Item():
+    def take(self):
+        print('item in inventory')
+
+
+class Figurewithitem(Figure, Item):
+    pass  # need a line? but want it empty? use pass
+
+
+f = Figurewithitem()
+f.move()
+f.take()
+
+# threading
+
+import threading
+
+
+class Messenger(threading.Thread):
+    def run(self):
+        for _ in range(1000):  # _ if i dont care about the variable just wanna loop x times
+            print(threading.currentThread().getName())
+
+
+x = Messenger(name='Sending messages')
+y = Messenger(name='Receive messages')
+x.start()  # with threading you dont use x.run() but start
+y.start()
+
+##unpacking lists
+date, item, price = ['December 24,2017', 'knife', 9.52]
+print(date)
+
+first = ['name1', 'name2', 'name3']
+last = ['name11', 'name22', 'name33']
+
+names = zip(first, last)
+for a, b in names:
+    print(a, b)
+
+# lambda
+
+
+def answer(x): return x * 7
+
+
+print(answer(5))
+
+example = {
+    'name1': 400,
+    'name2': 500,
+    'name3': 35.4,
+    'name4': 241,
+    'name5': 11
+}
+
+print(sorted(zip(example.values(), example.keys()))) #min/max/sorted
+
+# struct
+from struct import *
+# storing as bytes data
+packed_data = pack('iif', 6, 19, 4.76)
+print(packed_data)
+
+print(calcsize('if'))
+
+
+income = [20, 50, 60]
+
+
+def profit(dollar):
+    return dollar * 2
+
+
+new_income = list(map(profit, income))
+print(new_income)
+'''
+
